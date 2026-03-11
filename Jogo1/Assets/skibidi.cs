e97cb622a4c30afb3aa3966bbf97d5cb8031d099
Giovanni Florio Sannazzaro <giovannifloriosannazzarro@gmail.com>
2026-03-11 16:41:07 -0300

    criação do script

---
+++ Jogo1/Assets/skibidi.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class skibidi : MonoBehaviour
+{
+    public GameObject obj;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
