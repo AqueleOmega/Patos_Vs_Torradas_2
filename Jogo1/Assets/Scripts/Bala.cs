b29f405fd2e144d83e9a4af4fa7707e54555c65a
Giovanni Florio Sannazzaro <giovannifloriosannazzarro@gmail.com>
2026-03-25 16:42:39 -0300

    a bala vai pro jogador(e tentativa de ir para longe)
    vou para casa ent ta assim, não terminado msm

--- Jogo1/Assets/Scripts/Bala.cs
+++ Jogo1/Assets/Scripts/Bala.cs
@@ -5,6 +5,7 @@ public class Bala : MonoBehaviour
 {
     public Transform transJogador;
     public Transform transBala;
+    public GameObject bala;
     public Rigidbody2D tiro;
     public Vector2 posiçãoJogador;
 
@@ -16,13 +17,27 @@ public class Bala : MonoBehaviour
     
     void Esconder()
     {
-        tiro.SetActive(false);
+        bala.SetActive(false);
     }
-    void Mostrar()
-    {
 
-        transform.Translate(posiçãoJogador);
-        tiro.SetActive(true);
+    public void Atirar()
+    {
+        transform.position = posiçãoJogador;
+        bala.SetActive(true);
+        StartCoroutine(ExampleCoroutine());
+        bala.SetActive(false);
         
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
 }
