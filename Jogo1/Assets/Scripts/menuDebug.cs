022fcdd7a30ccb106bac9d3d1e74167613fc56a2
Giovanni Florio Sannazzaro <giovannifloriosannazzarro@gmail.com>
2026-03-29 00:54:22 -0300

    menu de debug
    fiz o menu de debug ( e descobrii sobre strings formatada) aperte f3 para descobrir

---
+++ Jogo1/Assets/Scripts/menuDebug.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class menuDebug : MonoBehaviour
+{
+    public TextMeshProUGUI fpsText;
+    public TextMeshProUGUI localização;
+    public TextMeshProUGUI texto3;
+    public Transform posição;
+    public Transform bala;
+    private float deltaTime = 0.0f;
+    int a = 0;
+    Vector2 posori;
+    Vector2 balas;
+
+    void Start()
+    {
+        fpsText.fontSize = 0;
+        localização.fontSize = 0;
+        texto3.fontSize = 0;
+
+    }
+    private void Update()
+    {
+        posori = posição.position;
+        balas = bala.position;
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = string.Format("FPS: {0}", fps);
+        localização.text = string.Format("X: {0}, Y: {0}", posori.x, posori.y);
+        //if ()
+        texto3.text = string.Format("X: {0}, Y: {0}", balas.x, balas.y);
+    }
+
+    public void Menu()
+    {
+        a += 1;
+        if (a == 1)
+        {
+            fpsText.fontSize = 40;
+            localização.fontSize = 40;
+            texto3.fontSize = 40;
+
+        }
+        if (a == 2)
+        {
+            a = 0;
+            fpsText.fontSize = 0;
+            localização.fontSize = 0;
+            texto3.fontSize = 0;
+        }
+                
+    }
+}
