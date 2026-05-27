53972b189e85caf1c1a6b9f9e41473dd5e8971ea
Giovanni Florio Sannazzaro <giovannifloriosannazzarro@gmail.com>
2026-05-27 01:04:52 -0300

    boss quase terminado

--- Jogo1/Assets/Scripts/inimigo.cs
+++ Jogo1/Assets/Scripts/inimigo.cs
@@ -1,29 +1,69 @@
+using JetBrains.Annotations;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Video;
+using static UnityEngine.GraphicsBuffer;
 
 public class inimigo : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float vida;
+    float tempoAtual = 0f;
+    float tempoTotal = 3f;
+    inimigo script;
+    public Transform jogador;
+    bool atk1;
+    public float strengh1;
+
     void Start()
     {
-        //rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        // andar
+        tempoAtual += Time.deltaTime;
+
+        if (atk1 == false)
+        {
+            Vector2 direcao = jogador.position - transform.position;
+            float angulo = Mathf.Atan2(direcao.y, direcao.x) * Mathf.Rad2Deg;
+
+            transform.eulerAngles = new Vector3(0, 0, angulo);
+        }
+
+
+        if (tempoAtual > tempoTotal)
+        {
+            StartCoroutine(Ataque_base());
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Bala"))
         {
-            //collision.transform.position = new Vector3(0, 1000, 0);
             vida -= 1;
             Debug.Log(vida);
         }
+        if (collision.gameObject.CompareTag("Parede"))
+        {
+            atk1 = false;
+            StopCoroutine(Ataque_base());
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+
+    IEnumerator Ataque_base()
+    {
+        atk1 = true;
+        while (atk1 == true)
+        {
+            rb.AddForce(transform.right * strengh1);
+            yield return new WaitForFixedUpdate();
+        }
+        tempoAtual = 0f;
     }
 }
--- Jogo1/Assets/Scripts/movimentação.cs
+++ Jogo1/Assets/Scripts/movimentação.cs
@@ -69,4 +69,6 @@ public class movimentação : MonoBehaviour
     }
 
 
+
+
 }
