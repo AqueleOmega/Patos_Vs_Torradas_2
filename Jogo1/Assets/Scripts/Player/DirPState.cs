d7abd7d429d4896d17e29953237a18a409a8b42d
Hypeonix <danielgoncalvesandrade.2010@gmail.com>
2026-07-01 23:39:44 -0300

    primeiros frames
    q lindo, os primeiros passos dos frames

--- Jogo1/Assets/Scripts/Player/DirPState.cs
+++ Jogo1/Assets/Scripts/Player/DirPState.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 public class DirPState : PlayerBaseState
 {
     public Animator head;
+
     public DirPState(Animator head1)
     {
         this.head = head1;
     }
+
     public override void EnterState(PlayerStateManager player)
     {
         head.SetBool("Direita", true);
-        Wait(player);
+
+        // CORREÇÃO: Usa o 'player' para iniciar a Corrotina
+        player.StartCoroutine(Wait(player));
     }
 
     public override void UpdateState(PlayerStateManager player)
     {
-        Debug.Log("Entrou");
+        // Debug.Log("Entrou"); // Cuidado com o spam no console aqui
     }
 
     public override void OnCollisionEnter(PlayerStateManager player, Collision collision)
     {
-         
+
     }
 
     public override void OnTriggerEnter(PlayerStateManager player, Collider2D collider)
     {
-       player.SwitchState(player.IdleState);
+        player.SwitchState(player.IdleState);
     }
 
-    private IEnumerator Wait(PlayerStateManager player) 
+    private IEnumerator Wait(PlayerStateManager player)
     {
         yield return new WaitForSeconds(0.8f);
         head.SetBool("Direita", false);
